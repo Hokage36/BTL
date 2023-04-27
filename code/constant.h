092d10c9cec6b27d662cb8ca14daee7cd7592e85
Hokage36 <xuansangtruongthi2201@gmail.com>
2023-04-27 15:47:34 +0700

    my commit

--- code/constant.h
+++ code/constant.h
@@ -3,6 +3,6 @@
 #include <iostream>
 const std::string WINDOW_TITLE = "Minesweeper!";
 
-const int SCREEN_WIDTH = 889;
+const int SCREEN_WIDTH = 900;
 const int SCREEN_HEIGHT = 500;
 const int TILE_SIZE = 28;
